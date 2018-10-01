fe136b6906ac334d221023a79650cf8118192d81
Mikko Marttila <mikmart@mikkomarttila.com>
2018-10-01 15:13:16 +0300

    Fix default alignment to have length 1

--- R/str-.R
+++ R/str-.R
@@ -3,8 +3,9 @@
 #' Align a vector of character strings by padding appropriately with spaces.
 #' Naturally fails hopelessly if the text is rendered in a non-monospace font.
 #'
-#' @param x a character vector
-#' @param align string specifying alignment
+#' @param x A character vector.
+#' @param align A character vector specifying alignment. Valid values are
+#'   `"left"`, `"right"`, and `"center"`.
 #' @return `x` with all elements padded to the maximum width and aligned
 #' @examples
 #' x <- c("short", "medium length", "and a rather long string")
@@ -15,7 +16,7 @@
 #' # can also use a vector of alignments
 #' str_align(x, c("center", "right", "left"))
 #' @export
-str_align <- function(x, align = c("left", "right", "center")) {
+str_align <- function(x, align = "left") {
   x <- as.character(x)
   n <- max(nchar(x))
   side <- align_to_side(align)
