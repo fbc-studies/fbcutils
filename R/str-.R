3e8a6fa60cb9aa9ef8e3dcf04a3300fddf564151
Mikko Marttila <mikmart@mikkomarttila.com>
2018-10-01 15:27:28 +0300

    Unvectorize align
    Most common use-case probably has a scalar align and in that case dropping the unnecessary pmap gives ~5x performance improvement.

--- R/str-.R
+++ R/str-.R
@@ -4,27 +4,25 @@
 #' Naturally fails hopelessly if the text is rendered in a non-monospace font.
 #'
 #' @param x A character vector.
-#' @param align A character vector specifying alignment. Valid values are
-#'   `"left"`, `"right"`, and `"center"`.
+#' @param align A string specifying alignment.
 #' @return `x` with all elements padded to the maximum width and aligned
 #' @examples
 #' x <- c("short", "medium length", "and a rather long string")
 #' str_align(x, "left")
 #' str_align(x, "right")
 #' str_align(x, "center")
-#'
-#' # can also use a vector of alignments
-#' str_align(x, c("center", "right", "left"))
 #' @export
-str_align <- function(x, align = "left") {
+str_align <- function(x, align = c("left", "right", "center")) {
+  align <- match.arg(align)
+
   x <- as.character(x)
   n <- max(nchar(x))
   side <- align_to_side(align)
 
-  # probably kinda slow to pmap but shouldn't matter most of the time
-  purrr::pmap_chr(list(x, n, side), stringr::str_pad)
+  # vectorizing align via pmap was 5x slower when align is scalar
+  stringr::str_pad(x, n, side)
 }
 
 align_to_side <- function(x) {
-  c(left = "right", right = "left", center = "both")[x]
+  switch(x, left = "right", right = "left", center = "both")
 }
