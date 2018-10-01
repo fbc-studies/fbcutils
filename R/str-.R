b33604e1da79f3b3f561f66aaa79e85216107e57
Mikko Marttila <mikmart@mikkomarttila.com>
2018-10-01 14:57:17 +0300

    Vectorize align

--- R/str-.R
+++ R/str-.R
@@ -1,4 +1,8 @@
 #' Align a vector of character strings
+#'
+#' Align a vector of character strings by padding approriately with spaces.
+#' Naturally fails hopelessly if text is rendered in a non-monospace font.
+#'
 #' @param x a character vector
 #' @param align string specifying alignment
 #' @return `x` with all elements padded to the maximum width and aligned
@@ -7,14 +11,17 @@
 #' str_align(x, "left")
 #' str_align(x, "right")
 #' str_align(x, "center")
+#'
+#' # can also use a vector of alignments
+#' str_align(x, c("center", "right", "left"))
 #' @export
 str_align <- function(x, align = c("left", "right", "center")) {
   x <- as.character(x)
   n <- max(nchar(x))
   side <- align_to_side(align)
 
-  ## TODO: vectorize align
-  stringr::str_pad(x, n, side)
+  # probably kinda slow to pmap but shouldn't matter most of the time
+  purrr::pmap_chr(list(x, n, side), stringr::str_pad)
 }
 
 align_to_side <- function(x) {
