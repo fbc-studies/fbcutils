b48ad2fbcb3cece51c38fcd2e56357b36a6ed10d
Mikko Marttila <mikmart@mikkomarttila.com>
2019-02-03 16:21:07 +0200

    Add np_gt()

--- R/np.R
+++ R/np.R
@@ -19,3 +19,32 @@ np <- function(df, x, ...) {
     mutate(p = n / sum(n), n_ryhma = sum(n)) %>%
     ungroup()
 }
+
+#' Create an np table and display it with gt
+#' @inheritParams np
+#' @param .fmt_p a gt formatter applied to the `p` column
+#' @export
+#' @importFrom magrittr %>%
+#' @examples
+#' np_gt(mtcars, cyl)
+#' np_gt(mtcars, cyl, am)
+#' np_gt(mtcars, cyl, am, vs)
+np_gt <- function(df, x, ..., .fmt_p = gt::fmt_percent) {
+  if (!requireNamespace("gt", quietly = TRUE)) {
+    stop("the gt package must be installed to use np_gt().", call. = FALSE)
+  }
+
+  quo_x <- rlang::enquo(x)
+  quos_dots <- rlang::quos(...)
+
+  np_tbl <- np(df, !!quo_x, !!!quos_dots)
+
+  np_tbl %>%
+    dplyr::select(-n_ryhma) %>%
+    dplyr::group_by(!!!quos_dots) %>%
+    gt::gt(rowname_col = rlang::as_label(quo_x)) %>%
+    .fmt_p("p") %>%
+    gt::summary_rows(TRUE, "n", fns = "sum", decimal = 0) %>%
+    gt::summary_rows(TRUE, "p", fns = "sum", formatter = .fmt_p) %>%
+    gt::tab_options(row.padding = "10px", summary_row.padding = "10px")
+}
