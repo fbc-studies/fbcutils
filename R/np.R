8c1a977c31c969f7f47fc66f541f55dde09f0b3b
Mikko Marttila <mikmart@mikkomarttila.com>
2019-02-03 15:28:37 +0200

    Document np

--- R/np.R
+++ R/np.R
@@ -1,3 +1,13 @@
+#' Calculate counts and percentages
+#'
+#' Wraps [dplyr::count()] to calculate counts and group percentages for a given
+#' combination of variables.
+#'
+#' @param df  a data frame
+#' @param x   a variable whose levels are counted
+#' @param ... additional variables to count by, defining also the grouping that
+#'   is used to calculate percentages
+#'
 #' @export
 np <- function(df, x, ...) {
   count_var <- enquo(x)
@@ -7,5 +17,5 @@ np <- function(df, x, ...) {
     group_by(!!!group_var) %>%
     count(!!count_var) %>%
     mutate(p = n / sum(n), n_ryhma = sum(n)) %>%
-    ungroup
+    ungroup()
 }
